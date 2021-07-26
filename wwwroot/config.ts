4413c997d4ed9b337b00fa981ffdcd839783b8b5
Kulteam <Kulteam@users.noreply.github.com>
2021-07-26 10:12:05 +0700

    Update config.ts

--- wwwroot/config.ts
+++ wwwroot/config.ts
@@ -1,22 +1,22 @@
 let global : any = typeof window !== 'undefined' ? window : self;
 global.config = {
     //apiUrl: typeof window !== 'undefined' && window.location ? window.location.href.substr(0, window.location.href.lastIndexOf('/') + 1) + 'api/' : 'https://wallet.plenteum.com/api/',
-    apiUrl: 'https://ple.bot.tips/', //temporary testing front end
-	mainnetExplorerUrl: "http://block-explorer.plenteum.com",
-    coinUnitPlaces: 8,
-    coinDisplayUnitPlaces: 2,
-	txMinConfirms: 20,         
-	txCoinbaseMinConfirms: 20, 
-	addressPrefix: 18181,
-	integratedAddressPrefix: 18181,
+    apiUrl: 'https://wallet.moncoin.io/api/', //temporary testing front end
+	mainnetExplorerUrl: "https://exp.moncoin.io",
+    coinUnitPlaces: 9,
+    coinDisplayUnitPlaces: 9,
+	txMinConfirms: 2,         
+	txCoinbaseMinConfirms: 2, 
+	addressPrefix: 2199681,
+	integratedAddressPrefix: 2199681,
 	feePerKB: new JSBigInt('0'), 
-	dustThreshold: new JSBigInt('1000000'),//used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
-	defaultMixin: 0, // default value mixins
+	dustThreshold: new JSBigInt('10'),//used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
+	defaultMixin: 3, // default value mixins
 	idleTimeout: 30,
 	idleWarningDuration: 20,
-	coinSymbol: 'PLE',
-	coinName: 'Plenteum',
-	coinUriPrefix: 'plenteum:',
-	avgBlockTime: 120,
+	coinSymbol: 'MON',
+	coinName: 'MONCoin',
+	coinUriPrefix: 'moncoin:',
+	avgBlockTime: 30,
 	maxBlockNumber: 500000000,
 };
